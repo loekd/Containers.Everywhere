9f54203e9020d56fe1048bcb88363bb86d5bd2c6
Loek <admin@admin>
2017-11-03 10:52:07 +0100

    remove connect

--- DeployServiceFabricCompose.ps1
+++ DeployServiceFabricCompose.ps1
@@ -7,18 +7,18 @@
      $ComposeFilePath = "./docker-compose-asf.yml",
      [Parameter(Mandatory=$False)]
      [string]
-     $dockerRegistryEndpointName ="ContainerRegistry",
+     $DockerRegistryEndpointName ="ContainerRegistry",
      [Parameter(Mandatory=$False)]
      [string]
-     $certThumbPrint = "D77444F0BD5B9CAAC0C0C9960478C2F771E461C2",
+     $CertThumbPrint = "D77444F0BD5B9CAAC0C0C9960478C2F771E461C2",
      [Parameter(Mandatory=$False)]
      [string]
-     $clusterEndpoint = "containerseverywhere.westeurope.cloudapp.azure.com:19000"
+     $ClusterEndpoint = "containerseverywhere.westeurope.cloudapp.azure.com:19000"
  )
 
  #for local testing
  function Connect(){
-    Connect-ServiceFabricCluster -ConnectionEndpoint $clusterEndpoint -X509Credential -StoreLocation CurrentUser -FindType FindByThumbprint -FindValue $certThumbPrint -ServerCertThumbprint $certThumbPrint 
+    Connect-ServiceFabricCluster -ConnectionEndpoint $ClusterEndpoint -X509Credential -StoreLocation CurrentUser -FindType FindByThumbprint -FindValue $CertThumbPrint -ServerCertThumbprint $CertThumbPrint 
  }
 
  #remove existing compose deployment and wait for completion
@@ -40,26 +40,26 @@
  #Rollout new compose deployment
  function Deploy(){
     
-    $dockerRegistryEndpointName = Get-VstsInput -Name $dockerRegistryEndpointName -Require
-    $dockerRegistryEndpoint = Get-VstsEndpoint -Name $dockerRegistryEndpointName -Require
+    $DockerRegistryEndpointName = Get-VstsInput -Name $DockerRegistryEndpointName -Require
+    $dockerRegistryEndpoint = Get-VstsEndpoint -Name $DockerRegistryEndpointName -Require
     $authParams = $dockerRegistryEndpoint.Auth.Parameters
     $username = $authParams.username
     $password = $authParams.password
     New-ServiceFabricComposeDeployment -DeploymentName $DeploymentName -Compose $ComposeFilePath -RegistryUserName $username -RegistryPassword $password
 }
 
-$connected = Get-ServiceFabricApplication -ErrorAction Continue
-if(!$connected)
-{
-    Connect
-}
-
-$connected = Get-ServiceFabricApplication -ErrorAction Continue
-if(!$connected)
-{
-    Write-Error "Failed to connect to cluster"
-    exit
-}
+#$connected = Get-ServiceFabricApplication -ErrorAction Continue
+#if(!$connected)
+#{
+#    Connect
+#}
+#
+#$connected = Get-ServiceFabricApplication -ErrorAction Continue
+#if(!$connected)
+#{
+#    Write-Error "Failed to connect to cluster"
+#    exit
+#}
 
 Remove
 Deploy
